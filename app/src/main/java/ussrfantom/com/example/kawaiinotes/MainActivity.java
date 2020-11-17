1dc764c4ce3da41533e16cfd70c48c75603c97c0
Ussrfantom <dimkakrohin@mail.ru>
2020-11-17 21:53:57 +0300

    add SQL DB

--- app/src/main/java/ussrfantom/com/example/kawaiinotes/MainActivity.java
+++ app/src/main/java/ussrfantom/com/example/kawaiinotes/MainActivity.java
@@ -6,7 +6,10 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,12 +21,18 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNotes; //наш ресайклер вью
     public static final ArrayList<Note> notes = new ArrayList<>(); //массив обьектов, наших заметок
     private NotesAdapter adapter;
+    private NotesDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        dbHelper = new NotesDBHelper(this);
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+    /*
         if (notes.isEmpty()) {
             notes.add(new Note("парикмахер", "сделать прическу", "Понедельник", 2));
             notes.add(new Note("парикмахер", "сделать прическу", "Понедельник", 1));
@@ -31,7 +40,31 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("парикмахер", "сделать прическу", "Понедельник", 3));
             notes.add(new Note("парикмахер", "сделать прическу", "Понедельник", 1));
         }
-        adapter = new NotesAdapter(notes);
+
+        for (Note note : notes){
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriority());
+            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+        }
+*/
+        ArrayList<Note> notesFromDB = new ArrayList<>();
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null);
+        while (cursor.moveToNext()){
+            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Note note = new Note(title, description, dayOfWeek, priority);
+            notesFromDB.add(note);
+        }
+        cursor.close();
+
+
+
+        adapter = new NotesAdapter(notesFromDB);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
         adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
---
+++ app/src/main/java/ussrfantom/com/example/kawaiinotes/NotesDBHelper.java
@@ -0,0 +1,31 @@
+package ussrfantom.com.example.kawaiinotes;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NotesDBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "notes.db";
+    private static final int DB_VERSION = 1;
+
+    public NotesDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+        //создание бд
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(NotesContract.NotesEntry.CREATE_COMMAND);
+    }
+        //удаление бд
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(NotesContract.NotesEntry.CREATE_COMMAND);
+        onCreate(db);
+    }
+}
